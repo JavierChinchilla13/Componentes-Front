a808b0ba093f50afc5224b5795ff5ea9a7e036a3
javi <javi@MSI-GF63-Thin>
2023-11-29 14:41:01 -0600

    Pequeños arreglos todavia falta

--- src/java/com/cci/ulatina/controller/CollaboratorController.java
+++ src/java/com/cci/ulatina/controller/CollaboratorController.java
@@ -11,6 +11,7 @@ import com.cci.manage.Empleados;
 import com.cci.ulatina.servicio.Servicio;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -124,7 +125,7 @@ public class CollaboratorController implements Serializable {
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar cumpleaños"));
             flag = false;
         }*/
-        /*if (this.selectedEmployee.getTelefono() == null || this.selectedEmployee.getTelefono().equals("")) {
+ /*if (this.selectedEmployee.getTelefono() == null || this.selectedEmployee.getTelefono().equals("")) {
             //ERROR
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar telefono"));
             flag = false;
@@ -139,13 +140,13 @@ public class CollaboratorController implements Serializable {
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar cedula"));
             flag = false;
         }
-        
+
         if (this.selectedEmployee.getCorreo() == null || this.selectedEmployee.getCorreo().equals("")) {
             //ERROR
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar correo"));
             flag = false;
         }
-        
+
         if (!checkEmail(this.selectedEmployee.getCorreo())) {
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar correo"));
             flag = false;
@@ -180,28 +181,168 @@ public class CollaboratorController implements Serializable {
             FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar tipo"));
             flag = false;
         }
-        
 
+        if (flag) {
+
+            System.out.println("Estoy salvando al usuario");
+            this.service.insertar(test.em, this.selectedEmployee);
+            this.esNuevo = false;
+            this.selectedEmployee = new Empleados();
+
+            PrimeFaces.current().executeScript("PF('manageUserDialog').hide()");
+        }
+
+        test.stopEntityManagerFactory();
+        System.out.println("YAAAA");
+    }
+
+    public void updateColaborador(int id) throws Exception {
+
+        test.startEntityManagerFactory();
+
+        boolean flag = true;
+
+        Empleados localizado = test.em.find(Empleados.class, new Integer(id));
+        if (localizado != null) {
+            System.out.println("Se localizo el empleado: " + localizado.getNombre());
+
+            if (this.selectedEmployee.getNombre() == null || this.selectedEmployee.getNombre().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar nombre"));
+                flag = false;
+            }
+            if (this.selectedEmployee.getApellido() == null || this.selectedEmployee.getApellido().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar apellido"));
+                flag = false;
+            }
+            /*if (this.selectedEmployee.getCumpleaños() == null || this.selectedEmployee.getCumpleaños().equals("")) {
+            //ERROR
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar cumpleaños"));
+            flag = false;
+        }*/
+ /*if (this.selectedEmployee.getTelefono() == null || this.selectedEmployee.getTelefono().equals("")) {
+            //ERROR
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar telefono"));
+            flag = false;
+        }*/
+            if (this.selectedEmployee.getDireccion() == null || this.selectedEmployee.getDireccion().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar direccion"));
+                flag = false;
+            }
+            if (this.selectedEmployee.getCedula() == null || this.selectedEmployee.getCedula().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar cedula"));
+                flag = false;
+            }
+
+            if (this.selectedEmployee.getCorreo() == null || this.selectedEmployee.getCorreo().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar correo"));
+                flag = false;
+            }
+
+            if (!checkEmail(this.selectedEmployee.getCorreo())) {
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar correo"));
+                flag = false;
+            }
+            if (this.selectedEmployee.getPassword() == null || this.selectedEmployee.getPassword().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar password"));
+                flag = false;
+            }
+            if (checkPassword(this.selectedEmployee.getPassword())) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR: Password format is incorrect", "The password must contain 4 to 8 characters and must contain numbers, lowercase and uppercase letters."));
+                flag = false;
+            }
+            if (this.selectedEmployee.getFechaIngreso() == null || this.selectedEmployee.getFechaIngreso().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar fecha de ingreso"));
+                flag = false;
+            }
+            /*if (this.selectedEmployee.getFechaRetiro() == null || this.selectedEmployee.getFechaRetiro().equals("")) {
+            //ERROR
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar fecha de retiro"));
+            flag = false;
+        }*/
+            if (this.selectedEmployee.getEstado() == null || this.selectedEmployee.getEstado().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar estado"));
+                flag = false;
+            }
+            if (this.selectedEmployee.getTipo() == null || this.selectedEmployee.getTipo().equals("")) {
+                //ERROR
+                FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Insertar tipo"));
+                flag = false;
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        } else {
+            System.out.println("No se encontro profesor");
+        }
+
+        if (flag) {
+            localizado.setNombre(this.selectedEmployee.getNombre());
+            localizado.setApellido(this.selectedEmployee.getApellido());
+            localizado.setCumpleaños(this.selectedEmployee.getCumpleaños());
+            localizado.setTelefono(this.selectedEmployee.getTelefono());
+            localizado.setDireccion(this.selectedEmployee.getDireccion());
+            localizado.setCedula(this.selectedEmployee.getCedula());
+            test.em.merge(localizado);
+            
+            System.out.println("Estoy salvando al usuario");
+            //this.service.modificar(test.em, this.selectedEmployee);
+            this.esNuevo = false;
+            this.selectedEmployee = new Empleados();
+
+            PrimeFaces.current().executeScript("PF('manageUserDialog').hide()");
+        }
+
+        test.stopEntityManagerFactory();
+        System.out.println("YAAAA");
+    }
+
+    public void deleteUser(int id) throws Exception {
+
+        boolean flag = true;
         
-        
+        Empleados localizado = test.em.find(Empleados.class, new Integer(id));
+			if(localizado != null) {
+				System.out.println("Se localizo el profesor: "+ localizado.getNombre());
+			}
+			else {
+				System.out.println("No se encontro profesor");
+                                
+                                flag = false;
+			}
 
         if (flag) {
             
-             
             
             
-            System.out.println("Estoy salvando al usuario");
-            this.service.insertar(test.em, this.selectedEmployee);    
+            localizado.setEstado("Inactivo");
+            
+            test.em.merge(localizado);
+
+            //java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+            
+            //this.service.modificar(test.em, this.selectedEmployee); 
+            //---this.servicioUsuario.listarUsuarios();
+            //this.listaUsuarios.add(selectedEmployee);//para simular       
             this.esNuevo = false;
             this.selectedEmployee = new Empleados();
-            
-                
-            
             PrimeFaces.current().executeScript("PF('manageUserDialog').hide()");
         }
-        
-        test.stopEntityManagerFactory();
-            System.out.println("YAAAA");
+
     }
     
     public boolean checkEmail(String email) {
